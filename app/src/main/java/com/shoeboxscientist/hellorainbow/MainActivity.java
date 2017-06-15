a48ffd83e47b2c83b065eb6c86145bd938b95b5e
Scott Piette <scottpiette@nestlabs.com>
2017-06-15 15:49:37 -0600

    Updated to use RainbowHat meta-driver   * Changed to new objects   * Refactored code to support new driver   * Removed BoardDefaults.java   * Removed RainbowHatConnector.java
    New RainbowHat meta-driver is located here -
    https://github.com/androidthings/contrib-drivers/tree/master/rainbowhat

--- app/src/main/java/com/shoeboxscientist/hellorainbow/MainActivity.java
+++ app/src/main/java/com/shoeboxscientist/hellorainbow/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.animation.AnimatorListenerAdapter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,13 +12,30 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 
+// import the RainbowHat driver
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+
+// import the devices on the RainbowHat
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.pwmspeaker.Speaker;
+import com.google.android.things.pio.Gpio;
+
+// For the Speaker
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.view.animation.LinearInterpolator;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private RainbowHatConnector mRainbowHat;
     private NanoHttpWebServer mServer;
     private String mHtmlTemplate;
     private int[] mRainbow = new int[7];
+    private static final int BRIGHTNESS = 1;
+    private int SPEAKER_READY_DELAY_MS = 300;
+    private Speaker mSpeaker;
+    private ValueAnimator mSpeakerAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +44,100 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Set up the rainbow hat
-        mRainbowHat = new RainbowHatConnector();
-        mRainbowHat.init();
-        mRainbowHat.makeNoise();
-        mRainbowHat.updateDisplay("HLLO");
-        mRainbowHat.updateLedStrip(new int[] { Color.BLACK, Color.BLACK, Color.BLACK,
+        // Setup the alphanumeric display
+        try {
+            AlphanumericDisplay segment = RainbowHat.openDisplay();
+            segment.setBrightness(BRIGHTNESS);
+            segment.display("HLLO");
+            segment.setEnabled(true);
+            // Close the device when done.
+            segment.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing alphanumeric display", e);
+        }
+
+        // Setup the rainbow
+        try {
+            Apa102 ledstrip = RainbowHat.openLedStrip();
+            ledstrip.setBrightness(BRIGHTNESS);
+            ledstrip.write(new int[] { Color.BLACK, Color.BLACK, Color.BLACK,
                 Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK});
+            // Close the device when done.
+            ledstrip.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing apa102 rainbow", e);
+        }
+
+        // Setup the Red LED
+        try {
+            Gpio Led = RainbowHat.openLedRed();
+            Led.setValue(false);
+            // Close the device when done.
+            Led.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing Red LED", e);
+        }
+
+        // Setup the Red LED
+        try {
+            Gpio Led = RainbowHat.openLedGreen();
+            Led.setValue(false);
+            // Close the device when done.
+            Led.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing Green LED", e);
+        }
+
+        // Setup the Blue LED
+        try {
+            Gpio Led = RainbowHat.openLedBlue();
+            Led.setValue(false);
+            // Close the device when done.
+            Led.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing Blue LED", e);
+        }
+
+        // Animator to make the speaker sound pretty.
+        mSpeakerAnimator = ValueAnimator.ofFloat(440, 440 * 4);
+        mSpeakerAnimator.setDuration(50);
+        mSpeakerAnimator.setRepeatCount(5);
+        mSpeakerAnimator.setInterpolator(new LinearInterpolator());
+        mSpeakerAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                try {
+                    float v = (float) animation.getAnimatedValue();
+                    // Open the device
+                    mSpeaker = RainbowHat.openPiezo();
+                    // Make noise
+                    mSpeaker.play(v);
+                    // Close the device when done.
+                    mSpeaker.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Error sliding speaker", e);
+                }
+            }
+        });
+
+        mSpeakerAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                try {
+                    // Open the device
+                    mSpeaker = RainbowHat.openPiezo();
+                    // Stop the buzzer.
+                    mSpeaker.stop();
+                    // Close the device when done.
+                    mSpeaker.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Error sliding speaker", e);
+                }
+            }
+        });
+
+        // Play a note on the buzzer.
+        mSpeakerAnimator.start();
 
         // Set up the Http server
         mHtmlTemplate = readHTMLFileTemplate();
@@ -97,6 +203,8 @@ public class MainActivity extends AppCompatActivity {
      */
     public String onHttpRequest(Map<String, String> params) {
         String resp = mHtmlTemplate;
+        boolean ledstate;
+
         if (params == null) {
             return resp;
         }
@@ -105,39 +213,90 @@ public class MainActivity extends AppCompatActivity {
         resp = resp.replace("$rainbow", convertNullToZero(params.get("rainbow")));
 
         if (params.containsKey("display")) {
-            mRainbowHat.updateDisplay(params.get("display"));
+            // Display a string on the segment display.
+            try {
+                AlphanumericDisplay segment = RainbowHat.openDisplay();
+                segment.setBrightness(BRIGHTNESS);
+                segment.display(params.get("display"));
+                // Close the device when done.
+                segment.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error initializing alphanumeric display", e);
+            }
 
             if (params.containsKey("rainbow")) {
-                mRainbowHat.updateLedStrip(getColours(Integer.parseInt(params.get("rainbow"))));
-                // Do It Twice since something w/ Android Things does not work consistently
-                mRainbowHat.updateLedStrip(getColours(Integer.parseInt(params.get("rainbow"))));
+                // Light up the rainbow.
+                try {
+                    Apa102 ledstrip = RainbowHat.openLedStrip();
+                    ledstrip.setBrightness(BRIGHTNESS);
+                    ledstrip.write(getColours(Integer.parseInt(params.get("rainbow"))));
+                    // Do It Twice since something w/ Android Things does not work consistently.
+                    ledstrip.write(getColours(Integer.parseInt(params.get("rainbow"))));
+                    // Close the device when done.
+                    ledstrip.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Error initializing apa102 rainbow", e);
+                }
             }
 
+            // Get the Red LED state.
             if (params.containsKey("redled")) {
-                mRainbowHat.setLED(true);
                 resp = resp.replace("$red", "checked");
+                ledstate = true;
             }
             else {
-                mRainbowHat.setLED(false);
                 resp = resp.replace("$red", "");
+                ledstate = false;
             }
 
+            // Set the Red LED.
+            try {
+                Gpio Led = RainbowHat.openLedRed();
+                Led.setValue(ledstate);
+                // Close the device when done.
+                Led.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error initializing Red LED", e);
+            }
+
+            // Get the Green LED state.
             if (params.containsKey("greenled")) {
-                mRainbowHat.setGreenLED(true);
                 resp = resp.replace("$green", "checked");
+                ledstate = true;
             }
             else {
-                mRainbowHat.setGreenLED(false);
                 resp = resp.replace("$green", "");
+                ledstate = false;
             }
 
+            // Set the Green LED.
+            try {
+                Gpio Led = RainbowHat.openLedGreen();
+                Led.setValue(ledstate);
+                // Close the device when done.
+                Led.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error initializing Green LED", e);
+            }
+
+            // Get the Blue LED state.
             if (params.containsKey("blueled")) {
-                mRainbowHat.setBlueLED(true);
                 resp = resp.replace("$blue", "checked");
+                ledstate = true;
             }
             else {
-                mRainbowHat.setBlueLED(false);
                 resp = resp.replace("$blue", "");
+                ledstate = false;
+            }
+
+            // Setup the Blue LED.
+            try {
+                Gpio Led = RainbowHat.openLedBlue();
+                Led.setValue(ledstate);
+                // Close the device when done.
+                Led.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error initializing Blue LED", e);
             }
         }
 
@@ -160,7 +319,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mRainbowHat.cleanup();
 
         if (mServer != null) {
             mServer.stop();
